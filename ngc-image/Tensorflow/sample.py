e4e6333a1f2aa253f8b638773185569c55ddd401
TheresaLiao <33051006+A60144@users.noreply.github.com>
2018-11-04 15:08:45 +0800

    Create sample.py

---
+++ ngc-image/Tensorflow/sample.py
@@ -0,0 +1,10 @@
+import time
+import tensorflow as tf
+hello = tf.constant('Hello, TensorFlow!')
+with tf.Session() as sess:
+    sess.run(hello)
+    a = tf.constant(10)
+    b = tf.constant(32)
+    sess.run(a+b)
+    while True:
+        time.sleep(5)
